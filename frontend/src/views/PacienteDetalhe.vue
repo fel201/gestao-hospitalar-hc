ff523b03eaf2c3d3ca0ea8824281ff030fdeacd7
Matheus_code <contatomh11@gmail.com>
2026-06-27 02:40:39 -0300

    chore: alterando texto do título do cabeçaho

--- frontend/src/views/PacienteDetalhe.vue
+++ frontend/src/views/PacienteDetalhe.vue
@@ -10,7 +10,7 @@
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
         </button>
 
-        <h1 class="text-2xl font-bold text-white mb-1">Plataforma de Jornada Assistencial</h1>
+        <h1 class="text-2xl font-bold text-white mb-1">DETALHES DA JORNADA DO PACIENTE</h1>
         <p class="text-sm text-slate-400 mb-6">Hospital das Clínicas - Perspectiva do Paciente</p>
         
         <div class="grid grid-cols-1 md:grid-cols-4 gap-4 text-sm">
