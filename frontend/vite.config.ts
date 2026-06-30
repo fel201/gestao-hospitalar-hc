d9feb514a7b890df2db19ff364a5a34b20c3cb95
Felipe Mateus <felipemateus216@gmail.com>
2026-06-30 18:17:03 -0300

    Configura vite.config.ts pro deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/static/dist/',
-  plugins: [vue(), tailwindcss()],
-  build: {
-    outDir: path.resolve(__dirname, '../src/static/dist'),
-    emptyOutDir: true,
-  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+
   server: {
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
-    }
-  }
+    },
+  },
 })
